7d73868b3df4b8f2f40e39f4fcb76655de5b83f9
arjunsoota <arjunsoota97@gmail.com>
2019-06-09 15:41:37 +0530

    need a username and password to log in, need to be logged in to add, edit, publish or delete posts, and can log out again.

--- newblog/settings.py
+++ newblog/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
